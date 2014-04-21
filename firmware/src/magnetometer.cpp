7ee03c550147fc425a5f018b2c4ce003e655160e
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-04-21 22:30:31 +0400

    Proper magnetometer driver

--- firmware/src/magnetometer.cpp
+++ firmware/src/magnetometer.cpp
@@ -5,8 +5,9 @@
  */
 
 #include "magnetometer.hpp"
-#include "board/hmc5883.h"
 #include "node.hpp"
+
+#include <array>
 
 #include <uavcan/equipment/ahrs/Magnetometer.hpp>
 
@@ -20,7 +21,8 @@ namespace magnetometer
 namespace
 {
 
-crdr_chibios::config::Param<float> param_variance("mag_variance", 0.1, 1e-6, 1.0);
+crdr_chibios::config::Param<float> param_variance("mag_variance_ga2", 0.005, 1e-6, 1.0);
+crdr_chibios::config::Param<unsigned> param_rate("mag_rate_hz", 10, 1, 30);
 
 void publish(float field[3], float variance)
 {
@@ -42,34 +44,113 @@ void publish(float field[3], float variance)
     (void)mag_pub.broadcast(mag);
 }
 
+template <unsigned TxSize, unsigned RxSize>
+bool io(const std::array<uint8_t, TxSize>& tx, std::array<uint8_t, RxSize>& rx)
+{
+    const unsigned Address = 0x1E;
+    i2cAcquireBus(&I2CD2);
+    const msg_t status = i2cMasterTransmitTimeout(&I2CD2, Address, tx.data(), TxSize, rx.data(), RxSize, MS2ST(5));
+    i2cReleaseBus(&I2CD2);
+    return status == RDY_OK;
+}
+
+bool tryInit()
+{
+    const uint8_t cfg_registers[] =
+    {
+        0b01111000, // Reg A: Averaging 8x, Update rate 75Hz, Normal mode
+        0b00100000, // Reg B: Default gain
+        0b00000000  // Mode: Continuous measurement
+    };
+    // Config write
+    {
+        std::array<uint8_t, 4> tx;
+        tx[0] = 0;
+        std::copy(cfg_registers, cfg_registers + 3, tx.begin() + 1);
+        std::array<uint8_t, 0> rx;
+        if (!io(tx, rx))
+        {
+            return false;
+        }
+    }
+    // Config readback - make sure it was written correctly
+    {
+        std::array<uint8_t, 1> tx;
+        tx[0] = 0;
+        std::array<uint8_t, 3> rx;
+        if (!io(tx, rx))
+        {
+            return false;
+        }
+        if (!std::equal(rx.begin(), rx.end(), cfg_registers))
+        {
+            return false;
+        }
+    }
+    // TODO: Sensor self-test
+    return true;
+}
+
+bool tryRead(float out_gauss[3])
+{
+    std::array<uint8_t, 1> tx;
+    tx[0] = 3;
+    std::array<uint8_t, 6> rx;
+    if (!io(tx, rx))
+    {
+        return false;
+    }
+
+    const int16_t hx = (((int16_t)rx[0]) << 8) | rx[1];  // X
+    const int16_t hz = (((int16_t)rx[2]) << 8) | rx[3];  // Z
+    const int16_t hy = (((int16_t)rx[4]) << 8) | rx[5];  // Y
+
+    out_gauss[0] = hx;  // TODO: gauss conversion
+    out_gauss[1] = hy;
+    out_gauss[2] = hz;
+    return true;
+}
+
+void transformToNEDFrame(float inout_mag_vector[3])
+{
+    const float x = -inout_mag_vector[1];
+    const float y = inout_mag_vector[0];
+    const float z = inout_mag_vector[2];
+
+    inout_mag_vector[0] = x;
+    inout_mag_vector[1] = y;
+    inout_mag_vector[2] = z;
+}
+
 class MagThread : public chibios_rt::BaseStaticThread<1024>
 {
-    HMC5883_t sensor = ::HMC5883_t();
-
 public:
     msg_t main() override
     {
-        lowsyslog("HMC5883: Initializing...\n");
-        while (hmc5883Init(&sensor, Avg4samp, SPS_15, MeasModeNormal, Gain1090, OmCont) < 0)
+        while (!tryInit())
         {
+            lowsyslog("Mag init failed, will retry...\n");
             ::sleep(1);
         }
-        lowsyslog("HMC5883: Init OK\n");
 
         const float variance = param_variance.get();
+        const uint64_t period_usec = 1000000 / param_rate.get();
+
+        systime_t sleep_until = chibios_rt::System::getTime();
 
         while (true)
         {
-            ::usleep(100000);
+            sleep_until += US2ST(period_usec);
 
-            auto meas = ::HMC5883meas_t();
-            (void)hmc5883ReadData(&sensor, &meas);
+            float vector[3] = {0, 0, 0};
+            if (tryRead(vector))
+            {
+                transformToNEDFrame(vector);
+                publish(vector, variance);
+            }
 
-            // TODO: TRANSFORM COORDINATE FRAME
-
-            publish(meas.h, variance);
+            chibios_rt::BaseThread::sleepUntil(sleep_until);
         }
-
         return msg_t();
     }
 } mag_thread;
