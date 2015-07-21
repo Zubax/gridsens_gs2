00286f43578585e6cc9d0941db068239a2c68bd8
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-07-21 22:59:48 +0300

    Linux test app update

--- tools/linux_test_app/subscriber.cpp
+++ tools/linux_test_app/subscriber.cpp
@@ -8,7 +8,8 @@
 #include <cassert>
 #include <stdexcept>
 #include <uavcan_linux/uavcan_linux.hpp>
-#include <uavcan/equipment/air_data/StaticAirData.hpp>
+#include <uavcan/equipment/air_data/StaticPressure.hpp>
+#include <uavcan/equipment/air_data/StaticTemperature.hpp>
 #include <uavcan/equipment/ahrs/Magnetometer.hpp>
 #include <uavcan/equipment/gnss/Fix.hpp>
 #include <uavcan/equipment/gnss/Auxiliary.hpp>
@@ -28,14 +29,7 @@ static uavcan_linux::NodePtr initNode(const std::vector<std::string>& ifaces, ua
 
     ENFORCE(0 <= node->start());
 
-    uavcan::NetworkCompatibilityCheckResult ncc_result;
-    ENFORCE(0 <= node->checkNetworkCompatibility(ncc_result));
-    if (!ncc_result.isOk())
-    {
-        throw std::runtime_error("Network conflict with node " + std::to_string(ncc_result.conflicting_node.get()));
-    }
-
-    node->setStatusOk();
+    node->setModeOperational();
     return node;
 }
 
@@ -55,7 +49,8 @@ makePrintingSubscriber(const uavcan_linux::NodePtr& node)
 static void runForever(const uavcan_linux::NodePtr& node)
 {
     auto sub_log      = makePrintingSubscriber<uavcan::protocol::debug::LogMessage>(node);
-    auto sub_air_data = makePrintingSubscriber<uavcan::equipment::air_data::StaticAirData>(node);
+    auto sub_pressure = makePrintingSubscriber<uavcan::equipment::air_data::StaticPressure>(node);
+    auto sub_temperat = makePrintingSubscriber<uavcan::equipment::air_data::StaticTemperature>(node);
     auto sub_mag      = makePrintingSubscriber<uavcan::equipment::ahrs::Magnetometer>(node);
     auto sub_gnss_fix = makePrintingSubscriber<uavcan::equipment::gnss::Fix>(node);
     auto sub_gnss_aux = makePrintingSubscriber<uavcan::equipment::gnss::Auxiliary>(node);
