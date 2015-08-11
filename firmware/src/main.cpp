fbad570439449bc99d5651c51fe9e5ece4cfbf8f
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-08-11 04:20:29 +0300

    Bootloader support

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -22,6 +22,20 @@
 
 namespace
 {
+/**
+ * This is the Brickproof Bootloader's app descriptor.
+ * Details: https://github.com/PX4/Firmware/tree/nuttx_next/src/drivers/bootloaders/src/uavcan
+ */
+const volatile class __attribute__((packed))
+{
+    std::uint8_t signature[8] = {'A','P','D','e','s','c','0','0'};
+    std::uint64_t image_crc = 0;
+    std::uint32_t image_size = 0;
+    std::uint32_t vcs_commit = GIT_HASH;
+    std::uint8_t major_version = FW_VERSION_MAJOR;
+    std::uint8_t minor_version = FW_VERSION_MINOR;
+    std::uint8_t reserved[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+} _app_descriptor __attribute__((section(".app_descriptor")));
 
 std::pair<unsigned, unsigned> getStatusLedOnOffMSecDurations()
 {
