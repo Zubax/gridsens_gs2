d516ce3012f3f5e3ae139416fd5dcb5250b21a47
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-08-12 22:47:48 +0300

    Binary generator fix

--- firmware/make_release_binaries.sh
+++ firmware/make_release_binaries.sh
@@ -8,7 +8,7 @@ SIGNED_BINARY="com.zubax.gnss-1.0-1.0.`git rev-parse --short=8 HEAD`.uavcan.bin"
 
 ./make_can_boot_descriptor.py -v build/zubax_gnss.bin build/$SIGNED_BINARY
 
-dd if=/dev/zero ibs=1k count=$BOOTLOADER_SIZE_KB | tr '\x00' '\xff' >build/padded_bootloader.bin
+dd if=/dev/zero ibs=1k count=$BOOTLOADER_SIZE_KB | tr "\000" "\377" >build/padded_bootloader.bin
 dd if=bootloader.bin of=build/padded_bootloader.bin conv=notrunc
 
 cat build/padded_bootloader.bin build/$SIGNED_BINARY > build/signed_with_bootloader.bin
