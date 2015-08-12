b2f8a317aae7fae0d91ce0f83f62cc07248988f3
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-08-12 22:28:52 +0300

    Final binary generation tools

---
+++ firmware/make_release_binaries.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+make clean
+make RELEASE=1 -j8
+
+BOOTLOADER_SIZE_KB=8
+SIGNED_BINARY="com.zubax.gnss-1.0-1.0.`git rev-parse --short=8 HEAD`.uavcan.bin"
+
+./make_can_boot_descriptor.py -v build/zubax_gnss.bin build/$SIGNED_BINARY
+
+dd if=/dev/zero ibs=1k count=$BOOTLOADER_SIZE_KB | tr '\x00' '\xff' >build/padded_bootloader.bin
+dd if=bootloader.bin of=build/padded_bootloader.bin conv=notrunc
+
+cat build/padded_bootloader.bin build/$SIGNED_BINARY > build/signed_with_bootloader.bin
