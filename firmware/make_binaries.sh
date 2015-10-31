59fd691275e3cb3e9a135fa7c2e5d83f84922535
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-10-31 11:24:07 +0300

    Makefile generates signed bins

--- firmware/make_binaries.sh
+++ firmware/make_binaries.sh
@@ -11,3 +11,28 @@ dd if=/dev/zero ibs=1k count=$BOOTLOADER_SIZE_KB | tr "\000" "\377" >build/padde
 dd if=bootloader.bin of=build/padded_bootloader.bin conv=notrunc
 
 cat build/padded_bootloader.bin build/$SIGNED_BINARY > build/signed_with_bootloader.bin
+
+#
+# Generating the combined ELF
+# Based on https://gist.github.com/tangrs/4030336
+#
+ld=`tempfile`
+tmp=`tempfile`
+cat > $ld <<EOF
+SECTIONS
+{
+EOF
+echo " . = 0x08000000;" >> $ld
+cat >> $ld <<EOF
+  .text : { *(.text) }
+}
+EOF
+
+CROSS_PREFIX=arm-none-eabi-
+
+${CROSS_PREFIX}ld -b binary -r -o $tmp build/signed_with_bootloader.bin
+${CROSS_PREFIX}objcopy --rename-section .data=.text --set-section-flags .data=alloc,code,load $tmp
+${CROSS_PREFIX}ld $tmp -T $ld -o build/signed_with_bootloader.elf
+${CROSS_PREFIX}strip -s build/signed_with_bootloader.elf
+
+rm -f $ld $tmp
