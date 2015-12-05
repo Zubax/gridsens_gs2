34955a1ac8e4bf4fd06a9f70c37f8c8c0361074a
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-12-05 11:08:33 +0300

    The dawn of drwatson

--- tools/drwatson/drwatson_zubax_gnss.py
+++ tools/drwatson/drwatson_zubax_gnss.py
@@ -16,4 +16,51 @@
 # Author: Pavel Kirienko <pavel.kirienko@zubax.com>
 #
 
-import drwatson
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], 'pyuavcan'))
+
+import uavcan
+from drwatson import init, run, make_api_context_with_user_provided_credentials, execute_shell_command,\
+    info, error, input, CLIWaitCursor, download, abort, glob_one, download_newest
+
+PRODUCT_NAME = 'com.zubax.gnss'
+DEFAULT_FIRMWARE_GLOB = 'https://files.zubax.com/products/%s/*.compound.bin' % PRODUCT_NAME
+CAN_BITRATE = 1000000
+
+
+args = init('''Zubax GNSS production testing application.
+Usage instructions:
+    1. Connect a CAN adapter to this computer.
+    2. Connect exactly one DroneCode Probe to this computer.
+    3. Start this application and follow its instructions.''',
+            lambda p: p.add_argument('iface', help='CAN interface or device path, e.g. "can0", "/dev/ttyACM0", etc.'),
+            lambda p: p.add_argument('--firmware', '-f', help='location of the firmware file (if not provided, ' +
+                                     'the firmware will be downloaded from Zubax Robotics file server)'),
+            require_root=True)
+
+dcp_port_gdb = glob_one('/dev/serial/by-id/*Black_Magic_Probe*-if00')
+dcp_port_cli = glob_one('/dev/serial/by-id/*Black_Magic_Probe*-if0[12]')
+
+use_socketcan = '/' not in args.iface
+
+api = make_api_context_with_user_provided_credentials()
+
+
+def get_firmware():
+    if args.firmware:
+        img = download(args.firmware)
+    else:
+        img = download_newest(DEFAULT_FIRMWARE_GLOB)
+    assert 30 < (len(img) / 1024) <= 240, 'Invalid firmware size'
+    return img
+
+
+with CLIWaitCursor():
+    # Downloading the firmware file
+    firmware = get_firmware()
+
+    # Initializing the CAN interface. If we're using SLCAN, entire initialization will be done by pyuavcan.
+    if use_socketcan:
+        execute_shell_command('ifconfig %s down && ip link set %s up type can bitrate %d sample-point 0.875',
+                              args.iface, args.iface, CAN_BITRATE, ignore_failure=True)
