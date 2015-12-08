c4e1b7cb2bf7a1dfa846bbab68fe8c1313bdb71f
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-12-08 09:21:09 +0300

    drwatson: CAN bitrate lowered to 125K because pyuavcan's SLCAN driver can't keep up with 1 Mbps data rate when running on a VirtualBox VM

--- tools/drwatson/drwatson_zubax_gnss.py
+++ tools/drwatson/drwatson_zubax_gnss.py
@@ -38,7 +38,7 @@ from functools import partial
 
 PRODUCT_NAME = 'com.zubax.gnss'
 DEFAULT_FIRMWARE_GLOB = 'https://files.zubax.com/products/%s/*.compound.bin' % PRODUCT_NAME
-CAN_BITRATE = 1000000
+CAN_BITRATE = 125000
 FLASH_OFFSET = 0x08000000
 TOOLCHAIN_PREFIX = 'arm-none-eabi-'
 DEBUGGER_PORT_GDB_GLOB = '/dev/serial/by-id/*Black_Magic_Probe*-if00'
