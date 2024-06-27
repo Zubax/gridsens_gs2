83ca2dfd357c9fcf7616e2d142b67ed4b99ed852
zen <110092745+Zentrus@users.noreply.github.com>
2024-06-27 15:15:32 +0300

    Force python3.8 (#28)
    updated scripts to force python3.8 otherwise it breaks

--- tools/drwatson/setup.sh
+++ tools/drwatson/setup.sh
@@ -6,6 +6,6 @@ if ! which arm-none-eabi-gdb; then
     echo "The toolchain does not seem to be installed correctly; please read this: https://kb.zubax.com/x/NoEh"
 fi
 
-sudo apt-get install -y python3-pip can-utils
+sudo apt-get install -y python3.8 python3.8-distutils can-utils || exit 1
 
-sudo pip3 install colorama easywebdav pyserial numpy pyyaml
+python3.8 -m pip install colorama easywebdav pyserial numpy pyyaml || exit 2
